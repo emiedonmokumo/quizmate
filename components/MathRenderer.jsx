5e9306cfb460a4848c9cf644de9c2b01d1bee730
Emie-Trialer <emiedonmukumo@gmail.com>
2024-10-08 15:54:01 +0100

    update

--- components/MathRenderer.jsx
+++ components/MathRenderer.jsx
@@ -1,11 +1,4 @@
-
-import katex from 'katex';
-
 const MathRenderer = ({ text }) => {
-//   const html = katex.renderToString(text, {
-//     throwOnError: false,
-//   });
-
   return <span dangerouslySetInnerHTML={{ __html: text }}></span>;
 };
 
